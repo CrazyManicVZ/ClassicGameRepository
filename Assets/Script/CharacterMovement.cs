b5b871b5e233dc741937d249e43b4550a3e8ce5e
CrazyManic <91173918+CrazyManic@users.noreply.github.com>
2022-09-24 03:30:33 +1000

    mainAudioTweenDone(fixingupBugs)

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -7,11 +7,13 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     private GameObject Player;
     private Tweener tweener;
+    public Animator playerAnimator;
+    public AudioSource walkingSource;
+    public AudioSource backgroundSource;
     public bool movingUp = false;
     public bool movingDown = false;
     public bool movingLeft = false;
     public bool movingRight = false; 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -25,28 +27,102 @@ public class CharacterMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+
+    void Tweening()
     {
         if (Player.transform.position == new Vector3(-13.5f, 3.50f, 0.0f))
         {
-            Tweenmovement(new Vector3(-13.5f, 7.50f, 0.0f), 3.0f);
-
+            Tweenmovement(new Vector3(-13.5f, 7.50f, 0.0f), 6.0f);
+            movingUp = true;
+            movingDown = false;
+            movingRight = false;
+            movingLeft = false; 
         }
 
         if (Player.transform.position == new Vector3(-13.5f, 7.50f, 0.0f))
         {
-            Tweenmovement(new Vector3(-8.5f, 7.50f, 0.0f), 3.0f);
+            movingUp = false;
+            movingDown = false;
+            movingRight = true;
+            movingLeft = false;
+            Tweenmovement(new Vector3(-8.5f, 7.50f, 0.0f), 6.0f);
         }
 
         if (Player.transform.position == new Vector3(-8.5f, 7.50f, 0.0f))
         {
-            Tweenmovement(new Vector3(-8.5f, 3.50f, 0.0f), 3.0f);
+            movingUp = false;
+            movingDown = true;
+            movingRight = false;
+            movingLeft = false;
+            Tweenmovement(new Vector3(-8.5f, 3.50f, 0.0f), 6.0f);
         }
 
         if (Player.transform.position == new Vector3(-8.5f, 3.50f, 0.0f))
         {
-            Tweenmovement(new Vector3(-13.5f, 3.50f, 0.0f), 3.0f);
+            movingUp = false;
+            movingDown = false;
+            movingRight = false;
+            movingLeft = true;
+            Tweenmovement(new Vector3(-13.5f, 3.50f, 0.0f), 6.0f);
         }
 
     }
+
+    void soundEffects()
+    {
+        if (movingUp == true || movingDown == true || movingLeft == true || movingRight == true)
+        {
+            backgroundSource.volume = 0.5f;
+            walkingSource.PlayDelayed(0.5f);
+        }
+        else
+        {
+            backgroundSource.volume = 1.0f;
+            walkingSource.Stop();
+        }
+    }
+
+    void playAnimation()
+    {
+        if (movingUp == true)
+        {
+            playerAnimator.SetBool("movingUp", true);
+            playerAnimator.SetBool("movingDown", false);
+            playerAnimator.SetBool("movingRight", false);
+            playerAnimator.SetBool("movingLeft", false);
+
+        }
+
+        if (movingDown == true)
+        {
+            playerAnimator.SetBool("movingUp", false);
+            playerAnimator.SetBool("movingDown", true);
+            playerAnimator.SetBool("movingRight", false);
+            playerAnimator.SetBool("movingLeft", false);
+        }
+
+        if (movingLeft == true)
+        {
+            playerAnimator.SetBool("movingUp", false);
+            playerAnimator.SetBool("movingDown", false);
+            playerAnimator.SetBool("movingRight", false);
+            playerAnimator.SetBool("movingLeft", true);
+        }
+
+        if (movingRight == true)
+        {
+            playerAnimator.SetBool("movingUp", false);
+            playerAnimator.SetBool("movingDown", false);
+            playerAnimator.SetBool("movingRight", true);
+            playerAnimator.SetBool("movingLeft", false);
+        }
+    }
+
+    void Update()
+    {
+        Tweening();
+        soundEffects();
+        playAnimation();
+    }
+
 }
