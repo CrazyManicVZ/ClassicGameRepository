6d3fd67943326f1363fad588f4cba637e8cf230c
CrazyManic <91173918+CrazyManic@users.noreply.github.com>
2022-10-25 03:45:26 +1100

    60% HUD done

---
+++ Assets/Script/PacStudentController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+    private Tweener tweener;
+    public Animator playerAnimator;
+    public AudioSource walkingSource;
+    public AudioSource backgroundSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
